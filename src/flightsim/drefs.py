a3d1e2e1a183cc417987b1ad7842824ddc4ddbf2
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2022-11-02 23:49:28 +0000

    Basic aircraft spawn

--- src/flightsim/drefs.py
+++ src/flightsim/drefs.py
@@ -25,6 +25,7 @@ class DREFs:
     lat = "sim/flightmodel/position/latitude"
     long = "sim/flightmodel/position/longitude"
     alt = "sim/flightmodel/position/elevation"  # elevation above MSL
+    cs_roll = "sim/joystick"
 
     class grafics:
         openglref_lat: str = "sim/flightmodel/position/lat_ref"
@@ -36,8 +37,8 @@ class DREFs:
         opengl_vy: str = "sim/flightmodel/position/local_vy"
         opengl_vz: str = "sim/flightmodel/position/local_vz"
         opengl_roll: str = "sim/flightmodel/position/phi"
-        oglpith: str = "sim/flightmodel/position/theta"  # True pitch
-        oglhdg: str = "sim/flightmodel/position/psi"  # OpenGL heading
+        opengl_pitch: str = "sim/flightmodel/position/theta"  # True pitch
+        opengl_hdg: str = "sim/flightmodel/position/psi"  # OpenGL heading
 
     class multiplayer:
         """
@@ -48,12 +49,19 @@ class DREFs:
 
         ```multiplayer.[DREF].format(i)```
         """
-        pos_x: str = "sim/multiplayer/position/plane{0}_x"
-        pos_y: str = "sim/multiplayer/position/plane{0}_y"
-        pos_z: str = "sim/multiplayer/position/plane{0}_z"
-        pitch: str = "sim/multiplayer/position/plane{0}_the"
-        roll: str = "sim/multiplayer/position/plane{0}_phi"
-        hdg: str = "sim/multiplayer/position/plane{0}_psi"
-        auto_alt: str = "sim/multiplayer/autopilot/altitude[{0}]"
-        auto_spd: str = ""
+        lon = "sim/multiplayer/position/plane1_lon"
+        lat = "sim/multiplayer/position"
+        opengl_x: str = "sim/multiplayer/position/plane{0}_x"
+        opengl_vx: str = "sim/multiplayer/position/plane{0}_v_x"
+        opengl_y: str = "sim/multiplayer/position/plane{0}_y"
+        opengl_vy: str = "sim/multiplayer/position/plane{0}_v_y"
+        opengl_z: str = "sim/multiplayer/position/plane{0}_z"
+        opengl_vz: str = "sim/multiplayer/position/plane{0}_v_z"
+        opengl_pitch: str = "sim/multiplayer/position/plane{0}_the"
+        opengl_roll: str = "sim/multiplayer/position/plane{0}_phi"
+        opengl_hdg: str = "sim/multiplayer/position/plane{0}_psi"
+        flap: str = "sim/multiplayer/controls/flap_request[{0}]"
+        # cs_pitch: str = "sim/multiplayer/position/plane{0}_yolk_pitch"
+        # cs_roll: str = "sim/multiplayer/position/plane{0}_yolk_roll"
+        # cs_yaw: str = "sim/multiplayer/position/plane{0}_yolk_yaw"
 
--- src/flightsim/leadergen.py
+++ src/flightsim/leadergen.py
@@ -5,9 +5,8 @@ __credits__ = []
 __email__ = "asc1g19@soton.ac.uk"
 __status__ = "Prototyping"
 
-from . import DREFs, Simulator
 from dataclasses import dataclass
-import numpy as np
+from . import Simulator, DREFs
 
 
 @dataclass(init=True)
@@ -39,16 +38,42 @@ class _Loader:
 
         self._get_flw_pos()
 
-    def load_leader(self):
-        self._gen_leader(self._flw, 1000, 500)
+    def load_leader(self, y_delta: float = 0):
+        # self._sim.update()
+        #
+        # a = _aircraft(x=self._sim.get(DREFs.grafics.opengl_x),
+        #                       y=self._sim.get(DREFs.grafics.opengl_y),
+        #                       z=self._sim.get(DREFs.grafics.opengl_z),
+        #                       hdg=self._sim.get(DREFs.grafics.opengl_hdg),
+        #                       pitch=self._sim.get(DREFs.grafics.opengl_pitch),
+        #                       roll=self._sim.get(DREFs.grafics.opengl_roll),
+        #                       vx=self._sim.get(DREFs.grafics.opengl_vx),
+        #                       vy=self._sim.get(DREFs.grafics.opengl_vy),
+        #                       vz=self._sim.get(DREFs.grafics.opengl_vz)
+        #                       )
 
-    def _gen_leader(self, leader: _aircraft, alt: float, speed: float) -> None:
-        self._sim.set(DREFs.multiplayer.pos_x.format(self._ldr_id), leader.x)
-        self._sim.set(DREFs.multiplayer.pos_y.format(self._ldr_id), leader.y)
-        self._sim.set(DREFs.multiplayer.pos_z.format(self._ldr_id), leader.z)
-        self._sim.set(DREFs.multiplayer.hdg.format(self._ldr_id), leader.hdg)
-        self._sim.set(DREFs.multiplayer.pitch.format(self._ldr_id), leader.pitch)
-        self._sim.set(DREFs.multiplayer.roll.format(self._ldr_id), leader.roll)
+        self._gen_leader(self._flw, 3000, y_delta)
+
+    def _gen_leader(self, leader: _aircraft, alt: float, y_delta) -> None:
+        # Y is height, Z is north, X is east (units not the same)
+        self._sim.set("sim/operation/override/override_autopilot[1]", True)
+
+        self._sim.set(DREFs.multiplayer.opengl_hdg.format(self._ldr_id), leader.hdg)
+        self._sim.set(DREFs.multiplayer.opengl_pitch.format(self._ldr_id), leader.pitch)
+        self._sim.set(DREFs.multiplayer.opengl_roll.format(self._ldr_id), leader.roll)
+        self._sim.set(DREFs.multiplayer.opengl_vx.format(self._ldr_id), leader.vx)
+        self._sim.set(DREFs.multiplayer.opengl_vy.format(self._ldr_id), leader.vy)
+        self._sim.set(DREFs.multiplayer.opengl_vz.format(self._ldr_id), leader.vz)
+        self._sim.set(DREFs.multiplayer.opengl_x.format(self._ldr_id), leader.x)
+        self._sim.set(DREFs.multiplayer.opengl_y.format(self._ldr_id), leader.y + y_delta)
+        self._sim.set(DREFs.multiplayer.opengl_z.format(self._ldr_id), leader.z)
+
+        # self._sim.set(DREFs.multiplayer.flap.format(self._ldr_id + 1), 0)
+
+        # self._sim.set(DREFs.multiplayer.cs_yaw.format(self._ldr_id), 0)
+        # self._sim.set(DREFs.multiplayer.cs_roll.format(self._ldr_id), 0)
+        # self._sim.set(DREFs.multiplayer.cs_pitch.format(self._ldr_id), 0)
+
 
     @property
     def _sim(self):
@@ -56,10 +81,11 @@ class _Loader:
             raise TypeError("Simulator not defined")
         else:
             return self._simv
+
     def _get_flw_pos(self):
         # TODO: For future proofing see https://github.com/uos-autonomous-uav-formation/control-system/issues/3
 
-        req = [DREFs.grafics.opengl_x, DREFs.grafics.opengl_y, DREFs.grafics.opengl_z, DREFs.grafics.oglhdg, DREFs.grafics.oglpith,
+        req = [DREFs.grafics.opengl_x, DREFs.grafics.opengl_y, DREFs.grafics.opengl_z, DREFs.grafics.opengl_hdg, DREFs.grafics.opengl_pitch,
                DREFs.grafics.opengl_vx, DREFs.grafics.opengl_vy, DREFs.grafics.opengl_vy]
         to_flush = []
         for i in req:
@@ -73,8 +99,8 @@ class _Loader:
         self._flw = _aircraft(x=self._sim.get(DREFs.grafics.opengl_x),
                               y=self._sim.get(DREFs.grafics.opengl_y),
                               z=self._sim.get(DREFs.grafics.opengl_z),
-                              hdg=self._sim.get(DREFs.grafics.oglhdg),
-                              pitch=self._sim.get(DREFs.grafics.oglpith),
+                              hdg=self._sim.get(DREFs.grafics.opengl_hdg),
+                              pitch=self._sim.get(DREFs.grafics.opengl_pitch),
                               roll=self._sim.get(DREFs.grafics.opengl_roll),
                               vx=self._sim.get(DREFs.grafics.opengl_vx),
                               vy=self._sim.get(DREFs.grafics.opengl_vy),
@@ -82,8 +108,8 @@ class _Loader:
                               )
 
         # Flush the DREFs we initialised (we only need them once)
-        for i in to_flush:
-            self._sim.add_freq_value(i, 0)
+        # for i in to_flush:
+        #     self._sim.add_freq_value(i, 0)
 
 
 class Cone(_Loader):
