3e94f4e6b700ee268ad9a6f56be55b7f431ffc72
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2022-11-10 19:55:03 +0000

    Basic multiplayer aircraft level flight

--- src/flightsim/multiplayercontrol.py
+++ src/flightsim/multiplayercontrol.py
@@ -53,31 +53,41 @@ class MultiplayerControl(Thread):
         self.id = id
         self._sim: Simulator = sim
 
-        self._controller_hdg = _PID(-0.004, 0, 0, 0)
+        self._controller_alt = _PID(-1, 0, 0, 0)
+        self._controller_roll = _PID(-1, 0, 0, 0)
+        self._controller_pitch = _PID(-1, 0, 0, 0)
+        self._controller_yaw = _PID(-1, 0, 0, 0)
         self._sim.add_freq_value(DREFs.multiplayer.elev.format(self.id), 60)
-        self._sim.add_freq_value(DREFs.multiplayer.lon.format(self.id), 60)
-        self._sim.add_freq_value(DREFs.multiplayer.lat.format(self.id), 60)
         self._sim.add_freq_value(DREFs.multiplayer.opengl_roll.format(self.id), 60)
+        self._sim.add_freq_value(DREFs.multiplayer.opengl_pitch.format(self.id), 60)
+        self._sim.add_freq_value(DREFs.multiplayer.opengl_hdg.format(self.id), 60)
 
-        super().__init__(target=self._control_system, args=(target_hdg, ))
+        super().__init__(target=self._control_system)
 
         self.start()
 
-    def _control_system(self, aircraft):
+    def _control_system(self):
+        vx = self._sim.get(DREFs.grafics.opengl_vx)
+        vz = self._sim.get(DREFs.grafics.opengl_vz)
 
         old_alt = self._sim.get(DREFs.multiplayer.elev.format(self.id))
-        old_lat = self._sim.get(DREFs.multiplayer.lat.format(self.id))
-        old_lon = self._sim.get(DREFs.multiplayer.lon.format(self.id))
-
-        vx = self._sim.get(DREFs.grafics.opengl_vx)
-        vy = self._sim.get(DREFs.grafics.opengl_vy)
-        vz = self._sim.get(DREFs.grafics.opengl_vz)
+        self._controller_alt.target = old_alt
 
         while True:
             self._sim.update()
 
+            new_alt = self._sim.get(DREFs.multiplayer.elev.format(self.id))
+            current_roll = self._sim.get(DREFs.multiplayer.opengl_roll.format(self.id))
+            current_hdg = self._sim.get(DREFs.multiplayer.opengl_hdg.format(self.id))
+            current_pitch = self._sim.get(DREFs.multiplayer.opengl_pitch.format(self.id))
+
+            print(f"{current_pitch=}, {current_hdg=}, {current_roll=}")
+
             self._sim.set(DREFs.multiplayer.opengl_vx.format(self.id), vx)
-            self._sim.set(DREFs.multiplayer.opengl_vy.format(self.id), 0)
+            self._sim.set(DREFs.multiplayer.opengl_vy.format(self.id), self._controller_alt.controller(new_alt))
+            self._sim.set(DREFs.multiplayer.cs_roll.format(self.id), self._controller_roll.controller(current_roll))
+            self._sim.set(DREFs.multiplayer.cs_hdg.format(self.id), self._controller_yaw.controller(current_hdg))
+            self._sim.set(DREFs.multiplayer.cs_pitch.format(self.id), self._controller_pitch.controller(current_pitch))
             self._sim.set(DREFs.multiplayer.opengl_vz.format(self.id), vz)
 
             # time.sleep(0.01)
