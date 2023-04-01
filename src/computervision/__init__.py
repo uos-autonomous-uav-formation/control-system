ccad5c943383b370542a6594145b47084535f084
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2023-04-01 18:40:04 +0100

    Moved analysis for analytical model of camera to separate repo

--- src/computervision/__init__.py
+++ src/computervision/__init__.py
@@ -5,7 +5,5 @@ MODEL_WEIGTS_DIR_CESSNA = "src\\ComputerVision\\models\\yolov4-tiny-c172.weights
 with open("src\\ComputerVision\\models\\classes.txt", "r") as f:
     CLASSES = f.readlines()
 
-
-
 from .screen import grab_screen_frame, liveframe
 from .objectdetection import ObjectDetection, load_img_from_file
